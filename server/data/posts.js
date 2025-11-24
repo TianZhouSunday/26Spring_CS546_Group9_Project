9b2ff732cd929758035305ddca8c1cf71194c856
TianZhouSunday <tzhou15@stevens.edu>
2025-11-24 16:34:05 -0500

    implement full CRUD operations and various query functions for posts

--- server/data/posts.js
+++ server/data/posts.js
@@ -1,5 +1,6 @@
 import helper from './helpers.js';
-import {posts} from "../config/mongoCollections.js";
+import { posts } from "../config/mongoCollections.js";
+import { ObjectId } from "mongodb";
 
 /**
  * Create a new post.
@@ -8,34 +9,33 @@ import {posts} from "../config/mongoCollections.js";
  */
 export const createPost = async (title, body, photo, location, sensitive, user) => {
     //check validation
-
     //check title
     title = helper.AvailableString(title, 'title');
-    if(title.length < 3 && title.length > 30) throw "Title length must between 3 and 30 characters.";
+    if (title.length < 3 && title.length > 30) throw "Title length must between 3 and 30 characters.";
 
     //check body(optional)
-    if(body) {
-        if(typeof body !== "string") throw "Post content must be a string.";
-        if(body.length > 500) throw "Post content cannot be over 500 characters.";
+    if (body) {
+        if (typeof body !== "string") throw "Post content must be a string.";
+        if (body.length > 500) throw "Post content cannot be over 500 characters.";
     }
 
     //check photo(optional)
-    if(photo) {
+    if (photo) {
         photo = helper.AvailableString(photo, 'photo path');
     }
 
     //check location
     helper.AvailableObj(location, 'location');
-    if(!location.hasOwnProperty('longitude')) throw "Location must contain longitude.";
-    if(!location.hasOwnProperty('latitude')) throw "Location must contain latitude.";
-    if(typeof location.longitude !== 'number' || location.longitude < -74.258 || location.longitude > -73.699) throw "Location must be in NYC";
-    if(typeof location.latitude !== 'number' || location.latitude < 40.496 || location.latitude > 40.916) throw "Location must be in NYC";
+    if (!location.hasOwnProperty('longitude')) throw "Location must contain longitude.";
+    if (!location.hasOwnProperty('latitude')) throw "Location must contain latitude.";
+    if (typeof location.longitude !== 'number' || location.longitude < -74.258 || location.longitude > -73.699) throw "Location must be in NYC";
+    if (typeof location.latitude !== 'number' || location.latitude < 40.496 || location.latitude > 40.916) throw "Location must be in NYC";
 
     //check sensitive
-    if(typeof sensitive !== 'boolean') throw "Sensitive must be a boolean.";
+    if (typeof sensitive !== 'boolean') throw "Sensitive must be a boolean.";
 
     //check user
-    user = helper.AvailableString(user, 'user');
+    user = helper.AvailableString(user, 'user ID');
 
 
     //create new post
@@ -55,10 +55,184 @@ export const createPost = async (title, body, photo, location, sensitive, user) 
         reports: []
     };
 
-    const postCollection = await posts();
-    const insertInfo = await postCollection.insertOne(newPost);
-    if (!insertInfo.acknowledged || !insertInfo.insertedId) throw "Failed to create post"
+    try {
+        const postCollection = await posts();
+        const insertInfo = await postCollection.insertOne(newPost);
+        if (!insertInfo.acknowledged || !insertInfo.insertedId) throw "Failed to create post"
 
-    console.log("Successfully created post", newPost);
-    return newPost;
+
+        console.log("Successfully created post", newPost);
+        return newPost;
+    } catch (e) {
+        console.error(e);
+        throw "Failed creating post in DB";
+    }
+}
+
+
+/**
+ * Get all existed posts
+ */
+export const getAllPosts = async () => {
+    try {
+        const postCollection = await posts();
+        return await postCollection.find({}).sort({date: -1}).toArray();
+    } catch (e) {
+        console.error(e);
+        throw "Failed getting all posts in DB";
+    }
+}
+
+
+/**
+ * Get a post by post ID.
+ * Required field: id.
+ */
+export const getPostById = async (id) => {
+    //check id
+    id = helper.AvailableID(id, "post ID");
+
+    //get user by id
+    try {
+        const postCollection = await posts();
+        const post = await postCollection.findOne({_id: new ObjectId(id)});
+
+        if (!post) throw 'Error: Post not found';
+
+        return post;
+    } catch (e) {
+        console.error(e);
+        throw "Failed getting post by post ID in DB";
+    }
+}
+
+
+/**
+ * Get posts by user ID.
+ * Required field: userId.
+ */
+export const getPostByUser = async (userId) => {
+    //check userId
+    userId = helper.AvailableID(userId, "user ID");
+
+    try {
+        const postCollection = await posts();
+        const userPosts = await postCollection.find({user: userId}).sort({date: -1}).toArray();
+
+        if (!userPosts) throw 'Error: Posts not found for this user';
+
+        return userPosts;
+    } catch (e) {
+        console.error(e);
+        throw "Failed getting posts by user ID in DB";
+    }
+}
+
+/**
+ * Get popular posts (sorted by post_score).
+ */
+export const getPopularPosts = async () => {
+    try {
+        const postCollection = await posts();
+        return await postCollection.find({}).sort({post_score: -1}).toArray();
+    } catch (e) {
+        console.error(e);
+        throw "Failed getting popular posts in DB";
+    }
+}
+
+
+/**
+ * update post
+ * Required fields: id, updatedPost
+ */
+export const updatePost = async (id, updatedPost) => {
+    //check validation
+    //check id
+    id = helper.AvailableID(id, "post ID");
+    //check updatedPost
+    helper.AvailableObj(updatedPost, "updated post");
+
+    //check every field
+    const updatedPostData = {};
+    //check title
+    if (updatedPost.title) {
+        let title = helper.AvailableString(updatedPost.title, 'title');
+        if (title.length < 3 && title.length > 30) throw "Title length must between 3 and 30 characters.";
+        updatedPostData.title = title;
+    }
+
+    //check body
+    if (updatedPost.body) {
+        let body = helper.AvailableString(updatedPost.body, 'body');
+        if (body.length > 500) throw "Post content cannot be over 500 characters.";
+        updatedPostData.body = body;
+    }
+
+    //check photo
+    if (updatedPost.photo) {
+        let photo = helper.AvailableString(updatedPost.photo, 'photo path');
+        updatedPostData.photo = photo;
+    }
+
+    //check location
+    if (updatedPost.location) {
+        let location = helper.AvailableObj(updatedPost.location, 'location');
+        if (!location.hasOwnProperty('longitude')) throw "Location must contain longitude.";
+        if (!location.hasOwnProperty('latitude')) throw "Location must contain latitude.";
+        if (typeof location.longitude !== 'number' || location.longitude < -74.258 || location.longitude > -73.699) throw "Location must be in NYC";
+        if (typeof location.latitude !== 'number' || location.latitude < 40.496 || location.latitude > 40.916) throw "Location must be in NYC";
+        updatedPostData.location = {
+            longitude: location.longitude,
+            latitude: location.latitude
+        };
+    }
+
+    //check sensitive
+    if (updatedPost.sensitive) {
+        let sensitive = helper.AvailableBoolean(updatedPost.sensitive, 'sensitive');
+        updatedPostData.sensitive = sensitive;
+    }
+
+    //check whether there are changes been input
+    if (Object.keys(updatedPostData).length === 0) throw "No inputs were found.";
+
+    //update post
+    try {
+        const postCollection = await posts();
+        const updatedInfo = await postCollection.updateOne({ _id: new ObjectId(id) }, { $set: updatedPostData });
+
+        if (updateInfo.matchedCount === 0) throw "No post was found with this ID.";
+        if (updateInfo.modifiedCount === 0) console.log("Warning: No changes were made.");
+
+        console.log("Post successfully updated!", updatedPostData);
+        return updatedPostData;
+    } catch (e) {
+        console.error(e);
+        throw "Failed updating post in DB.";
+    }
+}
+
+
+/**
+ * Delete a post by post ID.
+ * Required field: id.
+ */
+export const deletePost = async (id) => {
+    //check id
+    id = helper.AvailableID(id, "post ID");
+
+    //delete post
+    try {
+        const postCollection = await posts();
+        const deletedInfo = await postCollection.deleteOne({ _id: new ObjectId(id) });
+
+        if (deletedInfo.deletedCount === 0) throw "No post was found with this ID.";
+
+        console.log(`Post(ID: ${id}) successfully deleted!`);
+        return true;
+    } catch (e) {
+        console.error(e);
+        throw "Failed deleting post in DB.";
+    }
 }
